c5978d1185b3692047643bb926e39031441dae2c
dougfort <doug.fort@deciphernow.com>
2020-03-29 08:39:25 -0400

    make Traversal fields public (#3)

--- traversal.go
+++ traversal.go
@@ -10,8 +10,8 @@ import (
 
 // Traversal holds the state while travering JSON
 type Traversal struct {
-	err error
-	msg json.RawMessage
+	Err error
+	Msg json.RawMessage
 }
 
 // Start begins a Traversal by ijnitializing the internal state with JSON data.
@@ -19,9 +19,9 @@ func Start(data []byte) *Traversal {
 	var t Traversal
 
 	if json.Valid(data) {
-		t.err = t.msg.UnmarshalJSON(data)
+		t.Err = t.Msg.UnmarshalJSON(data)
 	} else {
-		t.err = errors.Errorf("Invalid JSON")
+		t.Err = errors.Errorf("Invalid JSON")
 	}
 
 	return &t
@@ -31,11 +31,11 @@ func Start(data []byte) *Traversal {
 // If there is no error, End writes the internal state to the writer
 // Note that a useful tool in debugging is to dump to os.Stdout
 func (t *Traversal) End(w io.Writer) error {
-	if t.err != nil {
-		return t.err
+	if t.Err != nil {
+		return t.Err
 	}
 
-	data, err := t.msg.MarshalJSON()
+	data, err := t.Msg.MarshalJSON()
 	if err != nil {
 		return errors.Wrap(err, "io.Write")
 	}
@@ -62,26 +62,26 @@ func (t *Traversal) End(w io.Writer) error {
 //
 // \"http\" (the output is JSON)
 func (t *Traversal) ObjectKey(key string) *Traversal {
-	if t.err != nil {
+	if t.Err != nil {
 		return t
 	}
 
-	m, err := GetMapFromRawMessage(t.msg)
+	m, err := GetMapFromRawMessage(t.Msg)
 	if err != nil {
 		return &Traversal{
-			err: errors.Wrap(err, "getMapFromRawMessage"),
-			msg: nil,
+			Err: errors.Wrap(err, "getMapFromRawMessage"),
+			Msg: nil,
 		}
 	}
 
 	msg, ok := m[key]
 	if !ok {
 		return &Traversal{
-			err: errors.Errorf("No entry for key '%s'", key),
-			msg: nil,
+			Err: errors.Errorf("No entry for key '%s'", key),
+			Msg: nil,
 		}
 	}
-	return &Traversal{err: nil, msg: msg}
+	return &Traversal{Err: nil, Msg: msg}
 }
 
 // ArraySingleton selects the only entry from an Array.
@@ -98,26 +98,26 @@ func (t *Traversal) ObjectKey(key string) *Traversal {
 // {"key": "value"}
 //
 func (t *Traversal) ArraySingleton() *Traversal {
-	if t.err != nil {
+	if t.Err != nil {
 		return t
 	}
 
-	s, err := GetSliceFromRawMessage(t.msg)
+	s, err := GetSliceFromRawMessage(t.Msg)
 	if err != nil {
 		return &Traversal{
-			err: errors.Wrap(err, "getSliceFromRawMessage"),
-			msg: nil,
+			Err: errors.Wrap(err, "getSliceFromRawMessage"),
+			Msg: nil,
 		}
 	}
 
 	if len(s) != 1 {
 		return &Traversal{
-			err: errors.Errorf("Array has %d items", len(s)),
-			msg: nil,
+			Err: errors.Errorf("Array has %d items", len(s)),
+			Msg: nil,
 		}
 	}
 
-	return &Traversal{err: nil, msg: s[0]}
+	return &Traversal{Err: nil, Msg: s[0]}
 }
 
 // ArrayPredicate selects an entry from an Array based on a predicate
@@ -149,26 +149,26 @@ func (t *Traversal) ArraySingleton() *Traversal {
 // {"key3": "value3"}
 //
 func (t *Traversal) ArrayPredicate(p func(json.RawMessage) bool) *Traversal {
-	if t.err != nil {
+	if t.Err != nil {
 		return t
 	}
 
-	s, err := GetSliceFromRawMessage(t.msg)
+	s, err := GetSliceFromRawMessage(t.Msg)
 	if err != nil {
 		return &Traversal{
-			err: errors.Wrap(err, "getSliceFromRawMessage"),
-			msg: nil,
+			Err: errors.Wrap(err, "getSliceFromRawMessage"),
+			Msg: nil,
 		}
 	}
 
 	for _, msg := range s {
 		if p(msg) {
-			return &Traversal{err: nil, msg: msg}
+			return &Traversal{Err: nil, Msg: msg}
 		}
 	}
 
 	return &Traversal{
-		err: errors.Errorf("No Array item satisfies the predicate: %s", t.msg),
-		msg: nil,
+		Err: errors.Errorf("No Array item satisfies the predicate: %s", t.Msg),
+		Msg: nil,
 	}
 }
